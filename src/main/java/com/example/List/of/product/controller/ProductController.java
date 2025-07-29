bb3e808ca13fe9c27bc0e8342d003ce16e463997
lovethukeachusim <ukeloveth247@gmail.com>
2025-07-29 18:30:57 +0100

    Revert to RequiredArgsConstructor annotation

--- src/main/java/com/example/List/of/product/controller/ProductController.java
+++ src/main/java/com/example/List/of/product/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.example.List.of.product.controller;
 import com.example.List.of.product.dto.ProductDto;
 import com.example.List.of.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +16,7 @@ import java.util.List;
 public class ProductController {
 
 
-    private ProductService productService;
-
-
-    @Autowired
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+    private final ProductService productService;
 
     @GetMapping("/getAll")
     public ResponseEntity<List<ProductDto>> getAll() {
